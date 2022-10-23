e2648ca41f6fd70414253524361ed11085f6da5c
cosmicwanderer7 <yewaleprithvi2003@gmail.com>
2022-10-23 14:17:56 +0530

    99% done

--- js/commands.js
+++ js/commands.js
@@ -61,28 +61,40 @@ social = [
 
 projects = [
   "<br>",
-  "<span class='underline'>Hey, I'm prithvi!👋</span>",
+  `<a href="https://github.com/cosmicwanderer7/google-clone" target="_blank"><span class='underline'>Google Search Clone</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">A functional replica of Google's search page, you can use it for searches. Styled with Tailwind CSS to Rapidly build and look as close as possible to current google search page, the search results are pulled using Googles Programmable Search Engine and it was build using Next.js the react framework.
+
+Project is live here <a href="https://google-clone-p8mn37r8a-cosmicwanderer7.vercel.app/" target="_blank">https://google-clone-cosmicwanderer7.vercel.app/</a>
+*Please do not spam searches.
+
+Built With
+
+    *Next.js
+    *Tailwind CSS
+    *Programmable Search Engine
+    *Yarn
+`,
+
   "<br>",
 
-  "<li>🔭 I’m currently a 3rd year undergrad, pursuing BSc.</li>",
-  "<li>🌱 My fields of intetest are</li>",
-  `<pre class="whitespace-pre-wrap">
-        - Fullstack Development
-        - UI/UX Designing</pre>`,
-  `<li><pre class="whitespace-pre-wrap">👯 I would love to work with anyone who wants to build a 
-   professional website for their business or even a personal 
-   website to showcase their portfolio.
-   I love learning new stuff and using it to improve my skills.
-   I primarily use NextJS and Tailwind CSS when building my 
-   websites (projects).
-   You can learn more about the projects that I have built using 
-   the project command.
-   You can browse around my website to find out more about 
-   me.
-   *Try using a social command.`,
-  `<li><pre class="whitespace-pre-wrap">👨‍💻 Check out my github profile to view my projects
-   *Use the github command to take a look at my GitHub profile.`,
-  `<li>📫 Contact me at <a href= "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRzDDPFDTbvhjVpvjpNxLZPjFdzKFcmqZGNHclhwtJKhdhrBqGNVGBzlPvFfwxvksdTcqHXs">yewaleprithvi2003@gmail.com</a>`,
+  `<a href="https://github.com/cosmicwanderer7/contactform" target="_blank"><span class='underline'>Contactform With Google Sheets as Database</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">This contact form collects information from the user and saves it to Google Sheets using the Google cloud APIs.
+After filling out the form, the user will receive a personalized email with a link to the form that says their response has been recorded. 
+It was built using Next.JS, the React framework and styled with Tailwind CSS.
+
+Project is live here <a href="https://contactform-psi.vercel.app/" target="_blank">https://contactform-psi.vercel.app/</a>
+
+Built With
+
+    *Next.js
+    *Tailwind CSS
+    *Google Sheets api
+    *IFTTT
+    *Yarn
+`,
+
   "<br>",
 ];
 
